e55ada9110080aeb64a90e0965a03577e121e203
Nabil Shafey <nabilshafey_68@yahoo.com>
2022-05-05 15:43:53 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+
+import Intro from './components/intro/Intro';
+import ProductList from './components/productList/ProductList';
+
+const App = () => {
+  return (
+    <div>
+      <Intro />
+      <About />
+      <ProductList />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: 'https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg',
+    link: 'http://lama.dev',
+  },
+  {
+    id: 2,
+    img: 'https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg',
+    link: 'http://lama.dev',
+  },
+  {
+    id: 3,
+    img: 'https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg',
+    link: 'http://lama.dev',
+  },
+  {
+    id: 4,
+    img: 'https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg',
+    link: 'http://lama.dev',
+  },
+  {
+    id: 5,
+    img: 'https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg',
+    link: 'http://lama.dev',
+  },
+  {
+    id: 6,
+    img: 'https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg',
+    link: 'http://lama.dev',
+  },
+];
